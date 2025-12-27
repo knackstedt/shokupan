95ae82995831b0df125adc02f6a6c9b82f23f34d
Andrew G. Knackstedt <knackstedt@outlook.com>
2025-12-27 15:39:34 -0500

    fix: set base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://knackstedt.github.io',
+    base: '/shokupan',
     integrations: [
         starlight({
             title: 'Shokupan',
