79cc3c2333bdd94a58a0670c64da2892d41e8d38
Andrew G. Knackstedt <knackstedt@outlook.com>
2026-01-06 16:42:42 -0500

    chore: move benchmarking folder

--- benchmarking/advanced-cases/shokupan.ts
+++ benchmarking/advanced-cases/shokupan.ts
@@ -1,6 +1,6 @@
-import { Compression } from "../../plugins/middleware/compression";
-import { Shokupan } from "../../shokupan";
-import type { Middleware, NextFn } from '../../util/types';
+import { Compression } from "../../src/plugins/middleware/compression";
+import { Shokupan } from "../../src/shokupan";
+import type { Middleware, NextFn } from '../../src/util/types';
 import { COMPRESSIBLE_JSON, LARGE_JSON, md5, serializeRequest } from "../advanced-data";
 
 export async function startAdvanced(port: number, scenario: string) {
