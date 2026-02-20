ff670193f7628a67da5d986a7f94949694b35599
Andrew G. Knackstedt <knackstedt@outlook.com>
2026-02-19 21:50:07 -0500

    chore: remove broken H3/QUIC

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import type { Shokupan } from './shokupan';
-import { BunAdapter, H3Adapter, NodeAdapter, type ServerAdapter } from './util/adapter';
+import { BunAdapter, NodeAdapter, type ServerAdapter } from './util/adapter';
 
 /**
  * Shokupan Server
@@ -47,7 +47,13 @@ export class ShokupanServer {
         } else if (adapterName === 'node') {
             adapter = new NodeAdapter();
         } else if (adapterName === 'h3') {
-            adapter = new H3Adapter();
+            console.warn(
+                '[Shokupan] ⚠️  The "h3" adapter is deprecated and has been removed.\n' +
+                '   HTTP/3 support is planned for a future release.'
+            );
+            throw new Error(
+                '[Shokupan] H3Adapter is no longer supported. HTTP/3 support is coming in a future release.'
+            );
         } else if (adapterName === 'wintercg') {
             throw new Error("WinterCG adapter does not support listen(). Use fetch directly.");
         } else {
